a31e7b3017649bdff2bee411879a5d7b818b429f
DimaRybalk <dima.rubaka1@gmail.com>
2026-08-12 02:24:38 +0200

    cache update

--- HW6/bookshop/books/signals.py
+++ HW6/bookshop/books/signals.py
@@ -9,3 +9,8 @@ def invalidation_book_cache(sender, instance, **kwargs):
     cache_key = f"book_detail:{instance.pk}"
     cache.delete(cache_key)
     print(f"cache #{instance.pk} successfully cleaned")
+
+
+@receiver([post_save, post_delete], sender=Book)
+def clear_books_cache(sender, instance, **kwargs):
+    cache.clear()
