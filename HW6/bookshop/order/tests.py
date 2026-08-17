6674595a5cc6994fbfc0bd047ccfcdfa77dba174
DimaRybalk <dima.rubaka1@gmail.com>
2026-08-17 06:42:28 +0200

    tests fix black

--- HW6/bookshop/order/tests.py
+++ HW6/bookshop/order/tests.py
@@ -6,18 +6,26 @@ from unittest.mock import patch, MagicMock
 from bookshop.factories import UserFactory, BookFactory, OrderFactory
 from unittest.mock import patch
 
+
 @pytest.fixture(autouse=True)
 def mock_warehouse_service():
-    with patch("bookshop.services.WarehouseClient.reserve_book") as mock_reserve, \
-         patch("bookshop.services.WarehouseClient.confirm_sale") as mock_confirm, \
-         patch("bookshop.services.WarehouseClient.release_reservation") as mock_release, \
-         patch("bookshop.services.WarehouseClient._get_auth_headers") as mock_auth:
-        
-        mock_auth.return_value = {"Authorization": "Bearer fake_token", "Host": "localhost"}
+    with patch("bookshop.services.WarehouseClient.reserve_book") as mock_reserve, patch(
+        "bookshop.services.WarehouseClient.confirm_sale"
+    ) as mock_confirm, patch(
+        "bookshop.services.WarehouseClient.release_reservation"
+    ) as mock_release, patch(
+        "bookshop.services.WarehouseClient._get_auth_headers"
+    ) as mock_auth:
+
+        mock_auth.return_value = {
+            "Authorization": "Bearer fake_token",
+            "Host": "localhost",
+        }
         mock_reserve.return_value = {"status": "reserved", "amount": 1}
         mock_confirm.return_value = {"status": "confirmed", "amount": 1}
         mock_release.return_value = {"status": "released", "amount": 1}
         yield
+
 
 @pytest.mark.django_db
 def test_order_creation_status():
